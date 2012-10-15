c3179e67f513ba8b2903208d2d1ad94919fcb950
Wallace Mooncai <wmooncai@alumni.rutgers.edu>
2012-10-14 23:52:11 -0400

    Initial Commit

---
+++ bma-week-6-xcode/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bma-week-6-xcode
+//
+//  Created by Wally on 10/14/12.
+//  Copyright (c) 2012 Wally. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
